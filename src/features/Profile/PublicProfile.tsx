97240554283a2c783248f4e01562cd5128e01896
Pratham <prathamtagad0@gmail.com>
2026-01-02 03:25:50 +0530

    Fixed profile error

--- src/features/Profile/PublicProfile.tsx
+++ src/features/Profile/PublicProfile.tsx
@@ -1,4 +1,4 @@
-wimport React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { useAuthStore } from '@stores/authStore'
 import { useUserStore } from '@stores/userStore'
